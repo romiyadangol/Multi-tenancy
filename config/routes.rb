d7b519a3f7ebb77886051bab5d5b734703dc7055
romiyadangol <dangolromiya@gmail.com>
2024-08-11 09:41:03 +0545

    install devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root "static_pages#landing_page"
   get 'dashboard', to: "static_pages#dashboard"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
